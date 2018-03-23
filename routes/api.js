4413186b7c1d5dcf236dc659b208af82947d9d5a
MCSmorenberg <mcsmorenberg@gmail.com>
2018-03-23 14:16:50 +0100

    update cors

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,7 @@ const Todo = require('../models/todo');
 // Enable CORS --> is a mechanism that uses additional HTTP headers to let a user agent gain permission.
 router.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
